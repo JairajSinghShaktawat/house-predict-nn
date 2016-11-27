e1f973d35c095296247f2b7f6b14f13570a5fa8b
Rohan Varma <rvarm1@gmail.com>
2016-11-27 02:15:38 -0800

    add a method to get the data as a matrix

--- parse_data.py
+++ parse_data.py
@@ -12,10 +12,21 @@ class DataParser(object):
                                 in THROW_AWAY}
         self.filtered_data = filtered_data
 
-    def get_data_as_dict(self):
+    def get_data(self):
         return self.filtered_data
+
+    def dict_to_matrix(self, list):
+        li = []
+        for dict_data in list:
+            features = [v for k, v in dict_data.iteritems() if k != "listSalePrice"]
+            features.append(dict_data["listSalePrice"])
+            li.append(features)
+        return li
+
 
 if __name__ == '__main__':
     THROW_AWAY = ['yearAge', 'filteredAddress', 'countyName', 'stateOrProvinceName', 'photoURL', 'MLSNumber', 'listingStatus', 'publicRemarks', 'siteMapDetailUrlPath', 'yearBuilt']
     parser = DataParser("sj.json", THROW_AWAY)
-    print parser.filtered_data
+    # print parser.filtered_data[0]
+    features_list = parser.dict_to_matrix(parser.get_data())
+    print len(features_list)
