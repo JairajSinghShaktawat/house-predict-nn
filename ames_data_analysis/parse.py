8e2ce0c45c32c0718008b6a424ed41004f64d1f7
Rohan Varma <rvarm1@gmail.com>
2016-12-10 15:08:37 -0800

    implement a custom categorical variable encoder

--- ames_data_analysis/parse.py
+++ ames_data_analysis/parse.py
@@ -1,50 +1,81 @@
 from sklearn import preprocessing
 import numpy as np
-np.set_printoptions(threshold='nan') #for array printing purposes
+np.set_printoptions(threshold='nan')  # for array printing purposes
 
 
-
-
-def generate_encoding(labels):
+def generate_encoding(features):
     """
     takes in a list of labels and returns a dictionary associating each label
     with a numerical value.
     params: labels - input list of labels to encode
     rtype: dict of labels associated with their numerical value
     """
-    label_dict = {labels[i]: i for i in range(0, len(labels))}
+    features = list(set(features))  # only unique labels
+    label_dict = {features[i]: i for i in range(0, len(features))}
     return label_dict
 
-def encode_labels(dataset, indices_to_encode=[]):
+
+def encode_features(dataset, indices_to_encode=[]):
     """
     takes in a numpy array and a list of indices to encode.
     generates an encoding for each of these values and reassigns the
     categorical variables to numerical variables
     """
+    ds = np.array(dataset)
     for index in indices_to_encode:
-        label_dict = generate_encoding(list(set(dataset[:, index])))
+        feature_dict = generate_encoding(dataset[:, index])
         for i in range(0, dataset.shape[0]):
-            dataset[i][index] = label_dict[dataset[i][index]]
-    return dataset
+            ds[i][index] = feature_dict[dataset[i][index]]
+    return ds
 
+
+def isint(x):
+    """return true if x is a string representing an int
+       params: x - string like
+       return: True if x is a string representing an int else False
+    """
+    for i in x:
+        if(i < '0' or i > '9'):
+            return False
+    return True
+
+
+def split_labels_from_dataset(dataset, label_idx=-1):
+    """
+    params: dataset - numpy array of data including labels
+            label_idx - index the label is at, for now supports 0 or -1 (last)
+    return: dataset: dataset without the labels, and the labels separetely
+    """
+    if(label_idx == -1):
+        labels = dataset[:, -1]
+        dataset = dataset[:, 0: -1]
+        return dataset, labels
+    else:
+        labels = dataset[0]
+        dataset = dataset[:, 1:]
+        return dataset, labels
 
 
 if __name__ == '__main__':
     fpath_descript = "../data/data_description.csv"
     fpath_train = "../data/house_train.csv"
     fpath_test = "../data/house_test.csv"
-    training_data = []
-    with open(fpath_train) as file:
-        training_data = file.readlines()
-    var_names = training_data[0]
-    var_names = var_names.split(",")
+    training_data = open(fpath_train).readlines()
+    var_names = training_data[0].split(",")
     training_data = training_data[1:]
-    dataset = []
-    for item in training_data:
-        dataset.append(item.split(","))
+    dataset = [item.split(",") for item in training_data]
     training_data = np.array(dataset)
-    encoding = generate_encoding(list(set(training_data[:, 2])))
-    print encoding
-    transformed_data = encode_labels(training_data, indices_to_encode=[2])
+    # extract out answers/labels from the array
+    training_data, prices = split_labels_from_dataset(training_data,
+                                                      label_idx=-1)
+    print prices.shape
+    print training_data.shape
+    encoding = generate_encoding(list(set(training_data[:, 5])))
+    encode_these = []
+    for i in range(len(training_data[0])):
+        if(not isint(training_data[0][i])):
+            encode_these.append(i)
+
+
+    transformed_data = encode_features(training_data, indices_to_encode=encode_these)
     print transformed_data[0]
-    enc = preprocessing.OneHotEncoder()
